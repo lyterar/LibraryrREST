f448bb18c077e8943948c7ccac0039dd1f3127d8
Andrei Socot <98881179+lyterar@users.noreply.github.com>
2025-07-20 20:31:20 +0300

    Add files via upload

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿namespace BookStore.Models;
+
+public class Book
+{
+    public const int MAX_TITLE_LENGTH = 100;
+    private Book(Guid id, string title, string description, decimal price)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Price = price;
+    }
+     
+    public Guid Id { get;  }
+    public string Title { get; } = string.Empty;
+    public string Description { get;  }=string.Empty;
+    public decimal Price { get;  }
+
+    public static (Book? Book, string Error) Create(Guid id, string title, string description, decimal price)
+    {
+        if (string.IsNullOrWhiteSpace(title) || title.Length > MAX_TITLE_LENGTH)
+        {
+            return (null, "Title cannot be empty or longer than 100 characters");
+        }
+    
+        var book = new Book(id, title, description, price);
+        return (book, string.Empty);
+    }
+
+}
